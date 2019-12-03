e4b65c5efa4a0cec4776669ee665fc72ed032a71
= <=>
2019-12-03 15:34:44 -0300

    Finished Forms module

--- angular-app/src/app/cart/cart.component.ts
+++ angular-app/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -10,14 +11,30 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
     items;
+    checkoutForm;
 
     constructor(
-        private cartService: CartService
-    ) { }
+        private cartService: CartService,
+        private formBuilder: FormBuilder
+    ) {
+        this.items = this.cartService.getItems();
+
+        this.checkoutForm = this.formBuilder.group({
+            name: '',
+            address: ''
+        });
+    }
 
     ngOnInit() {
         this.items = this.cartService.getItems();
         console.log(this.items);
+    }
+
+    onSubmit(customerData) {
+        console.warn('Your order has been submitted', customerData);
+
+        this.items = this.cartService.clearCart();
+        this.checkoutForm.reset();
     }
 
 }
